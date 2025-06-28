db390e373da85edde48f41e93138457466281f37
delvis Sanabria <134345133+DelvisSanabria@users.noreply.github.com>
2025-06-28 09:32:27 -0400

    add new excersises

--- Python-Basico/bucles.py
+++ Python-Basico/bucles.py
@@ -50,3 +50,48 @@ while choise.lower() == "iniciar":
     choise = "iniciar"  # Reinicia el bucle si el usuario escribe "continuar"
 #Al terminar el bucle, se imprime un mensaje de despedida.
 print("Gracias por usar la calculadora. ¡Hasta luego!")
+
+
+#Enunciado de ejercicios
+
+# Ejercicio 1: Contador Selectivo
+# Enunciado:  
+# Escribe un programa que pida al usuario un número entero positivo "n". Luego, imprime todos los números del 1 al "n" que cumplan al menos una de estas condiciones:
+# 1. El número es múltiplo de 3.
+# 2. El número es mayor que 10 y menor que 20.
+# 3. El número es igual a 25 o 50.  
+# Si el número no cumple ninguna condición, no se imprime.
+
+n = int(input("Ingrese un número entero positivo: "))
+print("Números que cumplen las condiciones:")
+for i in range(1, n + 1):
+    if (i % 3 == 0) or (10 < i < 20) or (i == 25 or i == 50):
+        print(i)
+
+
+# Ejercicio 2:
+#Enunciado: 
+# Crea un programa que simule la validación de una contraseña:
+# 1. Define una contraseña secreta (ej: "python123").
+# 2. Pide al usuario que ingrese una contraseña.
+# 3. El programa debe:
+#    - Permitir hasta 3 intentos.
+#    - Si la contraseña es correcta, muestra "¡Acceso concedido!" y terminar el programa.
+#    - Si es incorrecta, muestra "Contraseña incorrecta. Intentos restantes: X" y permitir un  siguiente intento.
+#    - Si se agotan los intentos, muestra "¡Cuenta bloqueada!".
+
+password = "python123"
+attents = 3
+
+
+while attents > 0:
+  input = input("Ingrese la contraseña: ")
+  if input == password:
+    print("¡Acceso concedido!")
+    break
+  else:
+    attents -= 1
+    print(f"Contraseña incorrecta. Intentos restantes: {attents}")
+
+if attents == 0:
+  print("¡Cuenta bloqueada!")
