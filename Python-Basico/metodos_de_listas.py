340061699bc357bbb47f6a7f4cc710d4b6f4558a
delvis Sanabria <134345133+DelvisSanabria@users.noreply.github.com>
2025-07-03 21:51:06 -0400

    add -new several class files

--- Python-Basico/metodos_de_listas.py
+++ Python-Basico/metodos_de_listas.py
@@ -17,6 +17,56 @@ print(names)
 
 #extend(): nos permite agregar varios elementos al final de la lista
 
-names.extend(["manuel","Andrea","Juan"])
+names.extend(["manuel","Andrea","Juan","Andres"])
 
 print(names)
+
+
+#insert(position, element): nos permite inserta un elemento, pero en una posicion especificada
+
+
+names.insert(2, "Andres")
+
+print(names)
+
+
+#remove(element): nos permite eliminar el primer elemento que coincida con el valor pasado como argumento
+
+names.remove("Andres")
+
+print(names)
+
+#index(element): nos devolvera la posicion del primer elemento que coincida con el valor pasado como argumento
+
+print(names.index("Paula"))
+
+#count(element): cuenta la cantidad de veces que se repite un elemento retornando un entero
+
+cellphones = ["iPhone 14", "iPhone 14 Pro", "iPhone 14 Pro Max", "iPhone 14", "iPhone 14 Pro", "iPhone 14 Pro Max"]
+
+print(cellphones.count("iPhone 14"))
+
+
+#sort(): nos permite ordenar una lista de menor a mayor (defecto)
+
+names.sort()
+
+print(names)
+
+numbers = [13,35,565,433,232,657,0,23,1,5]
+
+numbers.sort(reverse=True)
+
+print(numbers)
+
+#reverse(): nos permite invertir el orden de una lista
+
+names.reverse()
+
+print(names)
+
+#clear(): nos permite eliminar todos los elementos de una lista
+
+names.clear()
+
+print(f"La lista se encuentra vacia: {names}")
---
+++ Python-Basico/modularidad/main.py
@@ -0,0 +1,11 @@
+print("iniciando el modulo principal")
+
+import calculadora as cal
+
+sum = cal.sum(10, 5)
+
+rest = cal.rest(10, 5)
+
+print(f"La suma es: {sum}")
+
+print(f"La resta es: {rest}")
