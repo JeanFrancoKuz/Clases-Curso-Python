d7b304caa94619003e2b112a47a467b17426b845
delvis Sanabria <134345133+DelvisSanabria@users.noreply.github.com>
2025-07-04 21:41:07 -0400

    add -new several files

---
+++ Python-Basico/modularidad/helpers/herramientas.py
@@ -0,0 +1,11 @@
+def hello(name):
+  return print(f"Hola {name} ¿Que tal estas?")
+
+def goodbye(name):
+  return print(f"Adios {name} hasta pronto")
+
+def obtain_circle_area(radio):
+  import math
+  return print(math.pi * (radio ** 2))
+
+print("El modulo herramientas se ha cargado correctamente")
